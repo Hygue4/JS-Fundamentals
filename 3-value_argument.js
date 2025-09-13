6b209cf16b81acf44885b64e463cf6fcdc3660e1
Hygue4 <ndanyuzwehygue@gmail.com>
2025-09-14 00:16:41 +0300

    Add 3-value_argument script that prints the first argument or 'No argument'

---
+++ 3-value_argument.js
@@ -0,0 +1,12 @@
+const firstArg = process.argv[2]; // get the first argument after filename
+
+if (firstArg === undefined) {
+  console.log("No argument");
+} else {
+  console.log(firstArg);
+}
+
+
+
+
+    
